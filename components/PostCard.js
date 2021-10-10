6be3a66e906e53077c71fc7dc16a2962b4684900
Nancy Wan <nancy_wan@thecdm.ca>
2021-10-09 18:16:48 -0700

    update card styling

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,19 +2,25 @@ import React from "react"
 import Image from "next/image"
 import profile from "../public/profile.jpg"
 
-const PostCard = React.forwardRef(({href, onClick, date, title, description}, ref) => {
+const PostCard = React.forwardRef(({href, onClick, date, title, description, tags}, ref) => {
     return (
         <div class="max-w-4xl px-10 my-4 py-6 bg-white rounded-lg shadow-md">
         <div class="flex justify-between items-center">
             <span class="font-light text-gray-600">{date}</span>
-            <a class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500" href="#">Design</a>
+            <div class="flex flex-row-reverse space-x-1 space-x-reverse">
+            {
+                tags.map((tag, idx) => (
+                    <a key={idx} class="px-2 py-1 bg-gray-500 text-gray-100 font-bold rounded hover:bg-gray-500" href="#">{tag}</a>
+                ))
+            }
+            </div>
         </div>
         <div class="mt-2">
             <a class="text-2xl text-gray-700 font-bold hover:text-gray-600" ref={ref} href={href} onClick={onClick}>{title}</a>
             <p class="mt-2 text-gray-600">{description}</p>
         </div>
         <div class="flex justify-between items-center mt-4">
-            <a class="text-blue-600 hover:underline" href="#">Read more</a>
+            <a class="text-blue-600 hover:underline" ref={ref} href={href} onClick={onClick}>Read more</a>
             <div>
                 <a class="flex items-center" href="#">
                     <div className="mx-4">
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const ProjectCard = (props) => {
     return (
-        <div class="overflow-hidden shadow-lg rounded-lg h-auto w-80 md:w-80 cursor-pointer m-auto p-4 bg-indigo-50 transition duration-500 ease-in-out transform hover:translate-y-5 hover:shadow-2xl dark:bg-gray-900">
+        <div class="overflow-hidden shadow-lg rounded-lg h-auto w-80 md:w-80 cursor-pointer m-auto p-4 bg-indigo-200 transition duration-500 ease-in-out transform hover:translate-y-5 hover:shadow-2xl dark:bg-gray-900">
             <a class="w-full block h-full">
                 <img alt="cover image" src={props.coverImage?.url} class="max-h-60 w-full object-cover"/>
                 <div class="w-full">
@@ -17,7 +17,7 @@ const ProjectCard = (props) => {
                     <div class="flex flex-wrap justify-starts items-center mt-4 border-t-2 pt-5">
                         {
                             props.tags.map((item, idx) => (
-                            <div class="text-xs mr-2 py-1.5 px-4 text-gray-600 bg-blue-200 rounded-2xl dark:text-white" key={idx}>
+                            <div class="text-xs mr-2 py-1.5 px-4 text-gray-600 bg-yellow-100 rounded-2xl dark:text-white" key={idx}>
                                 {item}
                             </div>
                             ))
