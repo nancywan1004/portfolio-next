45034e97da446d7200efc5febf072c476ce16a90
Leqi(Nancy) Wan <46666414+nancywan1004@users.noreply.github.com>
2022-05-25 10:22:35 -0700

    Force deploy

--- components/Skills.js
+++ components/Skills.js
@@ -24,11 +24,6 @@ const Skills = () => {
             <div class=" w-full h-full bg-gray-200 absolute "></div>
             <div className=" animate-slideInFromLeft w-3/5 h-full bg-yellow-200 sm:bg-yellow-200 dark:bg-blue-200 absolute"></div>
         </div>
-        {/* <h2 class="pt-2 sm:pt-4 text-black dark:text-white font-mono">JavaScript</h2>
-        <div class="h-2 relative w-60 rounded-full overflow-hidden">
-            <div class=" w-full h-full bg-gray-200 absolute "></div>
-            <div class=" h-full bg-indigo-500 sm:bg-yellow-400 absolute" style={{width: "10%"}}></div>
-        </div> */}
         </div>
     )
 }
