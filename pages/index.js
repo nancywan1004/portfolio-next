247256c9b3aa6171b16aaffb9fd984cf35fdbf4c
Leqi(Nancy) Wan <nancywan1004@gmail.com>
2022-08-06 10:22:45 -0700

    Re-deploying with new resources

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function Home() {
       <title>Nancy's Portfolio</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
+  
     <main className="max-w-4xl mx-auto mt-16 antialiased">
       <About/>
     </main>
