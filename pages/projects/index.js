606c84466e294d221c6082f7ffb97126a9bbbc82
Leqi(Nancy) Wan <nancywan1004@gmail.com>
2022-08-06 10:04:02 -0700

    Re-deploying with new resources

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -15,7 +15,6 @@ export async function getStaticProps() {
 }
 
 export default function Projects({ portfolioItems }) {
-    //console.log(portfolioItems)
 
     const [ containerRef ] = useElementOnScreen({
       root: null,
