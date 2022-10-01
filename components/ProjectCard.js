799755850eabc934f934102ab10759e21bdc3e1f
nancywan_yumebau <nancy.wan@yumebau.com>
2022-09-30 22:01:25 -0700

    Fixing date file

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Image from "next/image"
 import Link from "next/link";
-import Date from "./Date";
+import Date from "./date";
 
 const ProjectCard = React.forwardRef(({href, onClick, title, description, tags, coverImage, startDate, endDate, content}, ref) => {
     return (
--- components/date.js
+++ components/date.js
@@ -1,4 +1,3 @@
-
 import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
