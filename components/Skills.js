ef162532da9082a8e8687c2581356b313677531a
nancywan_yumebau <nancy.wan@yumebau.com>
2023-05-13 20:47:59 -0700

    Moving css files and editing skills

--- components/Skills.js
+++ components/Skills.js
@@ -3,13 +3,13 @@ import React from "react";
 const Skills = () => {
     return (
         <div>
-        <h2 class="pt-2 sm:pt-4 text-black dark:text-white font-mono">ReactJS</h2>
+        <h2 class="pt-2 sm:pt-4 text-black dark:text-white font-mono">Unity, C#</h2>
         <div class="h-2 relative w-60 rounded-full overflow-hidden">
             <div className=" w-full h-full bg-gray-200 absolute "></div>
             <div className=" animate-slideInFromLeft w-full h-full bg-yellow-500 sm:bg-yellow-500 dark:bg-blue-500 absolute"></div>
         </div>
 
-        <h2 class="pt-2 sm:pt-4 text-black dark:text-white font-mono">Unity, C#</h2>
+        <h2 class="pt-2 sm:pt-4 text-black dark:text-white font-mono">Lua, JS/TS</h2>
         <div class="h-2 relative w-60 rounded-full overflow-hidden">
             <div class=" w-full h-full bg-gray-200 absolute "></div>
             <div className=" animate-slideInFromLeft w-11/12 h-full bg-yellow-400 sm:bg-yellow-400 dark:bg-blue-400 absolute"></div>
